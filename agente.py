b45eb20800d350bb32846a7f8545560a1218dbaa
cabeleno <64312851+cabeleno@users.noreply.github.com>
2026-02-28 12:32:56 +0100

    Update agente.py

--- agente.py
+++ agente.py
@@ -5,33 +5,28 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+o = Options()
+o.add_argument("--headless")
+o.add_argument("--no-sandbox")
+dr = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=o)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-wait = WebDriverWait(driver, 30)
+url = "https://www.astursalud.es/demandantes-empleo?p_p_id=AstursaludPortletBepe&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_AstursaludPortletBepe_article_id=56884"
+dr.get(url)
+time.sleep(10)
 
-driver.get("https://www.astursalud.es/demandantes-empleo?p_p_id=AstursaludPortletBepe&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_AstursaludPortletBepe_article_id=56884")
+sel_a = Select(dr.find_element(By.ID, "_AstursaludPortletBepe_area"))
+sel_a.select_by_visible_text("Area IV")
 
-area = wait.until(EC.presence_of_element_located((By.ID, "_AstursaludPortletBepe_area")))
-Select(area).select_by_visible_text("Area IV")
+sel_c = Select(dr.find_element(By.ID, "_AstursaludPortletBepe_categoria"))
+sel_c.select_by_visible_text("ENFERMERO/A-ATS/DUE")
 
-cat = driver.find_element(By.ID, "_AstursaludPortletBepe_categoria")
-Select(cat).select_by_visible_text("ENFERMERO/A-ATS/DUE")
-
-boton = driver.find_element(By.ID, "_AstursaludPortletBepe_consultarBusca")
-driver.execute_script("arguments[0].click();", boton)
+btn = dr.find_element(By.ID, "_AstursaludPortletBepe_consultarBusca")
+dr.execute_script("arguments[0].click();", btn)
 
 time.sleep(15)
-
-texto = driver.find_element(By.TAG_NAME, "body").text
-
+txt = dr.find_element(By.TAG_NAME, "body").text
 with open("resultado.txt", "w", encoding="utf-8") as f:
-    f.write(texto)
+    f.write(txt)
 
-driver.quit()
+dr.quit()
